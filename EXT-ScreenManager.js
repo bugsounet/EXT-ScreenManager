650ba50027e786a769d6b2477d82c7ad163980cf
bugsounet <github@bugsounet.fr>
2022-02-16 22:19:10 +0100

    first purpose ... for see

---
+++ EXT-ScreenManager.js
@@ -0,0 +1,103 @@
+/* Magic Mirror
+ * Module: ScreenManager
+ *
+ * By @bugsounet -- Dupont Cédric <bugsounet@bugsounet.fr>
+ * MIT Licensed.
+ */
+
+Module.register("EXT-ScreenManager", {
+  requiresVersion: "2.18.0",
+  defaults: {
+    debug: true,
+    ON: [ "0 * * * * *"],
+    OFF: [ "10 * * * * *"],
+    forceLock: true
+  },
+
+  start: function () {
+    this.Manager = {
+      assistantSpeak: false,
+      power: null,
+      wanted: true,
+      started: false
+    }
+    this.turnOffTimer = null
+  },
+
+  getDom: function() {
+    var dom = document.createElement("div")
+    dom.style.display = "none"
+    return dom
+  },
+
+  notificationReceived: function (notification, payload, sender) {
+    switch (notification) {
+      case "DOM_OBJECTS_CREATED":
+        this.sendSocketNotification("CONFIG", this.config)
+        this.sendSocketNotification("EXT_HELLO", this.name)
+        break
+      case "USER_PRESENCE":
+        if (payload && !this.Manager.started) {
+          if (!this.Manager.power && this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_LOCK")
+          this.Manager.power = true
+          this.Manager.started = true
+          if (this.config.forceLock) this.hideCounter()
+        }
+        break
+      case "SCREEN_POWER-ON":
+        if (!this.Manager.power && this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_LOCK")
+        this.Manager.power = true
+        if (!this.Manager.wanted) {
+          if(this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_LOCK")
+          this.turnOffbyTimer()
+        }
+        break
+      case "SCREEN_POWER-OFF":
+        this.Manager.power = false
+        break
+      case "ASSISTANT_LISTEN":
+      case "ASSISTANT_THINK":
+      case "ASSISTANT_REPLY":
+      case "ASSISTANT_CONTINUE":
+      case "ASSISTANT_CONFIRMATION":
+      case "ASSISTANT_ERROR":
+      case "ASSISTANT_HOOK":
+        clearTimeout(this.turnOffTimer)
+        this.turnOffTimer = null
+        break
+      case "ASSISTANT_STANDBY":
+        if (!this.Manager.wanted) this.turnOffbyTimer()
+        break
+    }
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    switch (notification) {
+      case "ON":
+        this.sendNotification("EXT_SCREEN-WAKEUP")
+        if (this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_LOCK")
+        this.Manager.wanted = true
+        break
+      case "OFF":
+        if (this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_UNLOCK")
+        this.sendNotification("EXT_SCREEN-END")
+        this.Manager.wanted = false
+        break
+    }
+  },
+
+  hideCounter: function() {
+    // force to hide EXT-Screen module (not needed)
+    var Screen = document.getElementById("EXT-SCREEN")
+    Screen.setAttribute('style', "display: none")
+  },
+
+  turnOffbyTimer: function() {
+    clearTimeout(this.turnOffTimer)
+    this.turnOffTimer = null
+    this.turnOffTimer= setTimeout(() => {
+      if (this.config.forceLock) this.sendNotification("EXT_SCREEN-FORCE_UNLOCK")
+      this.sendNotification("EXT_SCREEN-END")
+    }, 1000)
+  }
+});
--- node_helper.js
+++ node_helper.js
@@ -6,20 +6,13 @@
  */
 
 const NodeHelper = require("node_helper")
-const Screen = require("@bugsounet/screen")
-const Governor = require("@bugsounet/governor")
-const npmCheck = require("@bugsounet/npmcheck")
 var cron = require('node-cron')
 var log = (...args) => { /* do nothing */ }
 
 module.exports = NodeHelper.create({
   start: function () {
-    console.log("[MANAGER] MMM-ScreenManager Version:", require('./package.json').version)
+    console.log("[MANAGER] EXT-ScreenManager Version:", require('./package.json').version)
     this.config = null
-    this.screen = null
-    this.governor = null
-    this.useGovernor = false
-    this.Checker = null
   },
 
   socketNotificationReceived: function (notification, payload) {
@@ -35,37 +28,7 @@ module.exports = NodeHelper.create({
 
   initialize: function() {
     log("Initialize...")
-
-    /** npm library @bugsounet update checker **/
-    if (this.config.NPMCheck.useChecker) {
-      let npmConfig = {
-        dirName: __dirname,
-        moduleName: this.name,
-        timer: this.getUpdateTime(this.config.NPMCheck.delay),
-        debug: this.config.debug
-      }
-      this.Checker= new npmCheck(npmConfig, update => { this.sendSocketNotification("NPM_UPDATE", update)})
-    }
-
-    /** Governor library **/
-    this.useGovernor = this.config.governor.useGovernor
-    if (this.useGovernor) {
-      let governorConfig = {
-        useCallback: false,
-        sleeping: this.config.governor.sleeping,
-        working: this.config.governor.working
-      }
-      this.governor = new Governor(governorConfig, (noti, value) => { log("cb", noti,value) }, this.config.debug)
-      this.governor.start()
-    }
-
-    /** Screen library **/
-    let screenConfig = {
-      mode: this.config.screenMode
-    }
-    this.screen = new Screen(screenConfig, (noti, value) => { log("cb", noti,value) }, this.config.debug)
-    if (this.config.turnOnStart) this.turnOn()
-
+    // really !? LOL
     log("Initialized!")
 
     /** main process **/
@@ -78,7 +41,6 @@ module.exports = NodeHelper.create({
       this.config.ON.forEach(on => {
         if (!cron.validate(on)) return console.error("[MANAGER] Error event ON:", on)
         cron.schedule(on, () => {
-          if (this.useGovernor) this.governor.working()
           this.turnOn()
         })
         log("Added event ON:", on)
@@ -90,60 +52,19 @@ module.exports = NodeHelper.create({
         if (!cron.validate(off)) return console.error("[MANAGER] Error event OFF:", off)
         cron.schedule(off, () => {
           this.turnOff()
-          if (this.useGovernor) this.governor.sleeping()
         })
         log("Added event OFF:", off)
       })
     } else log ("No event OFF")
   },
 
-  /** Screen Manager with @bugsounet/screen library... wow too complex :D **/
   turnOn: function() {
-    log("Turn ON Screen")
-    this.screen.wantedPowerDisplay(true)
-    this.sendSocketNotification("SCREEN_PRESENCE", true)
+    log("Send: Turn ON Screen")
+    this.sendSocketNotification("ON")
   },
 
   turnOff: function() {
-    log("Turn OFF Screen")
-    this.screen.wantedPowerDisplay(false)
-    this.sendSocketNotification("SCREEN_PRESENCE", false)
-  },
-
-  /** convert h m s to ms **/
-  getUpdateTime: function(str) {
-    let ms = 0, time, type, value
-    let time_list = ('' + str).split(' ').filter(v => v != '' && /^(\d{1,}\.)?\d{1,}([wdhms])?$/i.test(v))
-
-    for(let i = 0, len = time_list.length; i < len; i++){
-      time = time_list[i]
-      type = time.match(/[wdhms]$/i)
-
-      if(type){
-        value = Number(time.replace(type[0], ''))
-
-        switch(type[0].toLowerCase()){
-          case 'w':
-            ms += value * 604800000
-            break
-          case 'd':
-            ms += value * 86400000
-            break
-          case 'h':
-            ms += value * 3600000
-            break
-          case 'm':
-            ms += value * 60000
-            break
-          case 's':
-            ms += value * 1000
-          break
-        }
-      } else if(!isNaN(parseFloat(time)) && isFinite(time)){
-        ms += parseFloat(time)
-      }
-    }
-    return ms
-  },
-
+    log("Send: Turn OFF Screen")
+    this.sendSocketNotification("OFF")
+  }
 });
